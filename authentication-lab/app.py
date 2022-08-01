686db8914611934872f6770ebdbe0d36155436bc
MaysAb23-meet <109899447+MaysAb23-meet@users.noreply.github.com>
2022-08-01 12:22:50 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -9,13 +9,14 @@ config = {
   "storageBucket": "fb-proj-5f40b.appspot.com",
   "messagingSenderId": "145320707025",
   "appId": "1:145320707025:web:8b3e9912bcb0ef0f0b16f0",
-  "measurementId": "G-944E7PESXZ", "databaseURL":""
+  "measurementId": "G-944E7PESXZ", "databaseURL":"https://fb-proj-5f40b-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -42,9 +43,15 @@ def signup():
     if request.method == 'POST':
        email = request.form['email']
        password = request.form['password']
-       return redirect(url_for('add_tweet'))
+       full_name = request.form['full_name']
+       username = request.form['username']
+       bio = request.form['bio']
+
+       user = {"full_name": "full_name", "username": "username","bio":"bio"}
        try:
             login_session['user'] =  auth.create_user_with_email_and_password(email, password)
+            db.child("Users").child(login_session['user']['localId']).set(user)
+            
             return redirect(url_for('add_tweet'))
        except:
            error = "authentication failed"
@@ -55,7 +62,22 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+
+        tweet = {"text": text, "title": title,"username":(db.child("user")[localId]).get().val()["username"]}
+        try:
+            db.child("tweet").push(tweet)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "authentication failed"
     return render_template("add_tweet.html")
+
+
+
+   
+
 
 
 @app.route('/signout')
